26b7a24c9bdf26ba989d0c7c45769479e0eb7568
Fuji125 <fujianto556@gmail.com>
2025-04-25 10:32:06 +0700

    Perbaikan pada seluru fitur dan menambahkan profil pada navbar

--- js/main.js
+++ js/main.js
@@ -120,17 +120,17 @@
     });
     
     
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
+    // selflearning isotope and filter
+    var selflearningIsotope = $('.selflearning-container').isotope({
+        itemSelector: '.selflearning-item',
         layoutMode: 'fitRows'
     });
 
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('filter-active');
+    $('#selflearning-flters li').on('click', function () {
+        $("#selflearning-flters li").removeClass('filter-active');
         $(this).addClass('filter-active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        selflearningIsotope.isotope({filter: $(this).data('filter')});
     });
     
 })(jQuery);
